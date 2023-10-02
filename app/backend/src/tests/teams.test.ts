ffd2964a81909b007a00c6aff8a441fa58088ee2
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-10-02 19:49:32 -0300

    Requisito 4 - TDD endpoint /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -1,41 +1,33 @@
 import * as chai from 'chai';
 import * as sinon from 'sinon';
 import { app } from '../app';
-import Team from '../database/models/TeamsModel';
+import SequelizeTeams from '../database/models/SequelizeTeams';
+import { mockAllTeams, mockIdTeam } from './mocks/teamsMocks';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
-
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-
-const mockAllTeams = [
-  {
-    "id": 1,
-    "teamName": "Avaí/Kindermann"
-  },
-  {
-    "id": 2,
-    "teamName": "Bahia"
-  },
-  {
-    "id": 3,
-    "teamName": "Botafogo"
-  },
-]
-
-describe('Testando a rota teams', () => {
+describe('Testando a rota /teams', () => {
 
   it('Verifica se retorna todos os times', async () => {
-    sinon.stub(Team, 'findAll').resolves(mockAllTeams as any);
+    sinon.stub(SequelizeTeams, 'findAll').resolves(mockAllTeams as any);
 
     const result = await chai.request(app).get('/teams');
     
     expect(result.status).to.equal(200);
     expect(result.body).to.deep.equal(mockAllTeams);
+  });
+
+  it('Verifica se retorna dados de um time específico', async () => {
+    sinon.stub(SequelizeTeams, 'findOne').resolves(mockIdTeam as any);
+
+    const result = await chai.request(app).get('/teams/:id');
+    
+    expect(result.status).to.equal(200);
+    expect(result.body).to.deep.equal(mockIdTeam);
   });
 
 });
