b3cd7fb5c363bcfcf2657e42c131ae0e3f02ed97
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-10-03 21:00:23 -0300

    Requisito 9 - TDD 20% de cobertura endpoint /login

--- app/backend/src/middlewares/validationsUser.ts
+++ app/backend/src/middlewares/validationsUser.ts
@@ -1,18 +1,37 @@
 import { NextFunction, Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+import JWT from '../utils/JWT';
 
 export default class ValidationLogin {
   static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
     const { email, password } = req.body;
 
-    if (!email || !password) return res.status(400).json({ message: 'All fields must be filled' });
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
 
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const emailValido = emailRegex.test(email);
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (!emailValido || password.length < 6) {
+    if (!emailRegex.test(email) || password.length < 6) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
+    next();
+  }
+
+  static validateToken(req: Request, res: Response, next: NextFunction): Response | void {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    const tokenNotExist = token.split(' ')[1];
+    const isValidToken = JWT.verify(tokenNotExist) as JwtPayload;
+
+    if (!isValidToken) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
     next();
   }
 }
